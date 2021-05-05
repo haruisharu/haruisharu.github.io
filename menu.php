742141341da0fe888a4ae3ab18f312b8efce0378
ELGA MARIATI <elgamariati.@gmail.com>
2021-05-05 22:09:26 +0700

    commit awal

---
+++ menu.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Pusat Studi Pertambangan dan Energi Baru Terbarukan - LPPM UPR
+    </title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- ======= Mobile nav toggle button ======= -->
+    <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+
+    <!-- ======= Header ======= -->
+    <header id="header">
+        <div class="d-flex flex-column">
+
+            <div class="profile">
+                <img src="assets/img/logo.png" alt="" class="img-fluid rounded-circle">
+                <h1 class="text-light"><a href="index.html">Pusat Studi</a></h1>
+                <h1 class="text-light" style="text-align:center; font-size:16px"><a href="index.html">Pertambangan dan Energi
+                        Baru
+                        Terbarukan</a></h1>
+
+                <!-- <div class="social-links mt-3 text-center">
+          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div> -->
+            </div>
+            <!-- <nav class="nav-menu ">
+        <ul>
+          <li class="active"><a href="index.html"><i class="bx bx-home"></i> <span>Beranda</span></a></li>
+          <li><a href="menu.php"><i class="bx bx-user"></i> <span>Profil</span></a></li>
+          <li><a href="#resume"><i class="bx bx-file-blank"></i> <span>Program Kerja</span></a></li>
+          <li><a href="#services"><i class="bx bx-server"></i>Kerja Sama</a></li>
+          <li><a class="uji" href=""><i class="bx bx-envelope"></i>Kontak</a><span class="caret"></span>
+            <ul class="nested">
+              <a href="#services"><i class="bx bx-circle"></i>Kerja Sama</a>
+              <a href="#services"><i class="bx bx-server"></i>Kerja Sama</a>
+            </ul>
+          </li>
+          <li><a href="#contact"><i class="bx bx-envelope"></i>Penelitian</a></li>
+          <li><a href="#services"><i class="bx bx-server"></i> Dokumen Resmi</a></li>
+          <li><a href="#contact"><i class="bx bx-envelope"></i>Kontak</a></li>
+
+
+        </ul>
+      </nav> -->
+
+            <nav class="nav-menu ">
+                <ul>
+                    <li class="active"><a href="index.html"><i class="bx bx-home"></i></a></li>
+                    <li><a href="menu.php"><span>Profil</span></a></li>
+                    <li><a href="#resume"><span>Program Kerja</span></a></li>
+                    <li><a href="#services">Kerja Sama</a></li>
+                    <li><a class="uji" href="">Kontak</a><span class="caret"></span>
+                        <ul class="nested">
+                            <a href="#services">Kerja Sama</a>
+                            <a href="#services">Kerja Sama</a>
+                        </ul>
+                    </li>
+                    <li><a href="#contact">Penelitian</a></li>
+                    <li><a href="#services">Dokumen Resmi</a></li>
+                    <li><a href="#contact">Kontak</a></li>
+
+
+                </ul>
+            </nav><!-- .nav-menu -->
+            <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+
+        </div>
+    </header><!-- End Header -->
+
+
+    <main id="main">
+
+        <!-- ======= Breadcrumbs ======= -->
+        <section class="breadcrumbs">
+            <div class="container">
+
+                <div class="d-flex justify-content-between align-items-center">
+                    <h2>Profil</h2>
+                    <ol>
+                        <li><a href="index.html">Home</a></li>
+                        <li>Profil</li>
+                    </ol>
+                </div>
+
+            </div>
+        </section><!-- End Breadcrumbs -->
+
+        <section class="inner-page">
+            <div class="container">
+                <p style="text-align: justify;">
+                    Dalam rangka meningkatkan ilmu pengetahuan dan kerjasama global diperlukan sumber daya manusia yang unggul, profesional, kompeten dan kompetitif. Kekayaan alam yang ada di KalTeng pada sektor pertambangan antara lain emas, tembaga, perak, intan, zircon, bijih besi, alumunium (bauksit), pasir kuarsa, kaolin, batu granit, batu gamping, batu andesit dan batubara.
+                    Pada tahun 2018 informasi dari Dinas Energi Sumber Daya Mineral (ESDM) Provinsi Kalimantan Tengah ada 1.083 perizinan tambang (IUP) yang telah terverifikasi/Clear and Clean (CnC) dan 566 izin usaha pertambangan yang beroperasi. IUP didominasi batubara pada wilayah timur, sedangkan wilayah barat terdapat IUP mineral non logam, emas, zircon, bijih besi, bauksit dan pasir kuarsa.
+                    Untuk mengembangkan pendidikan, riset, penelitian, dan pengabdian kepada masyarakat bagi dosen, mahasiswa dan stakeholder pertambangan maka perlu dibentuk “Pusat Studi Pertambangan dan Energi Baru Terbarukan” pada Lembaga Penelitian dan Pengabdian Kepada Masyarakat (LPPM) Universitas Palangka Raya
+                </p>
+            </div>
+        </section>
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong><span>LPPM UPR</span></strong>
+            </div>
+            <div class="credits">
+                Designed by <a href="http://lppm.upr.ac.id/" target="_blank">Tim IT LPPM UPR</a>
+            </div>
+        </div>
+    </footer>
+    <!-- End  Footer -->
+
+    <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+    <script src="assets/vendor/counterup/counterup.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/venobox/venobox.min.js"></script>
+    <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="assets/vendor/typed.js/typed.min.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+    <script>
+        var toggler = document.getElementsByClassName("caret");
+        var i;
+
+        for (i = 0; i < toggler.length; i++) {
+            toggler[i].addEventListener("click", function() {
+                this.parentElement.querySelector(".nested").classList.toggle("active");
+                this.classList.toggle("caret-down");
+            });
+        }
+    </script>
+
+</body>
+
+</html>
---
+++ semua-posting.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Pusat Studi Pertambangan dan Energi Baru Terbarukan - LPPM UPR
+    </title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <script src="https://use.fontawesome.com/536b1ce0a9.js"></script>
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- ======= Mobile nav toggle button ======= -->
+    <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+
+    <!-- ======= Header ======= -->
+    <header id="header">
+        <div class="d-flex flex-column">
+
+            <div class="profile">
+                <img src="assets/img/logo.png" alt="" class="img-fluid rounded-circle">
+                <h1 class="text-light"><a href="index.html">Pusat Studi</a></h1>
+                <h1 class="text-light" style="text-align:center; font-size:16px"><a href="index.html">Pertambangan dan Energi
+                        Baru
+                        Terbarukan</a></h1>
+
+                <!-- <div class="social-links mt-3 text-center">
+          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div> -->
+            </div>
+            <!-- <nav class="nav-menu ">
+        <ul>
+          <li class="active"><a href="index.html"><i class="bx bx-home"></i> <span>Beranda</span></a></li>
+          <li><a href="menu.php"><i class="bx bx-user"></i> <span>Profil</span></a></li>
+          <li><a href="#resume"><i class="bx bx-file-blank"></i> <span>Program Kerja</span></a></li>
+          <li><a href="#services"><i class="bx bx-server"></i>Kerja Sama</a></li>
+          <li><a class="uji" href=""><i class="bx bx-envelope"></i>Kontak</a><span class="caret"></span>
+            <ul class="nested">
+              <a href="#services"><i class="bx bx-circle"></i>Kerja Sama</a>
+              <a href="#services"><i class="bx bx-server"></i>Kerja Sama</a>
+            </ul>
+          </li>
+          <li><a href="#contact"><i class="bx bx-envelope"></i>Penelitian</a></li>
+          <li><a href="#services"><i class="bx bx-server"></i> Dokumen Resmi</a></li>
+          <li><a href="#contact"><i class="bx bx-envelope"></i>Kontak</a></li>
+
+
+        </ul>
+      </nav> -->
+
+            <nav class="nav-menu ">
+                <ul>
+                    <li class="active"><a href="index.html"><i class="bx bx-home"></i></a></li>
+                    <li><a href="menu.php"><span>Profil</span></a></li>
+                    <li><a href="#resume"><span>Program Kerja</span></a></li>
+                    <li><a href="#services">Kerja Sama</a></li>
+                    <li><a class="uji" href="">Kontak</a><span class="caret"></span>
+                        <ul class="nested">
+                            <a href="#services">Kerja Sama</a>
+                            <a href="#services">Kerja Sama</a>
+                        </ul>
+                    </li>
+                    <li><a href="#contact">Penelitian</a></li>
+                    <li><a href="#services">Dokumen Resmi</a></li>
+                    <li><a href="#contact">Kontak</a></li>
+
+
+                </ul>
+            </nav><!-- .nav-menu -->
+            <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+
+        </div>
+    </header><!-- End Header -->
+
+
+    <main id="main">
+
+        <!-- ======= Breadcrumbs ======= -->
+        <section class="breadcrumbs">
+            <div class="container">
+
+                <div class="d-flex justify-content-between align-items-center">
+                    <!-- <h2>Contoh judul postingan</h2> -->
+                    <ol>
+                        <li><a href="index.html">Home</a></li>
+                        <li>Semua Postingan</li>
+                    </ol>
+                </div>
+
+            </div>
+        </section><!-- End Breadcrumbs -->
+
+        <section class="inner-page">
+
+            <div class="container">
+                <div class="input-group rounded">
+                    <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+                    <span class="input-group-text border-0" id="search-addon" style="margin-left: 5px;">
+                        <i class="fa fa-search"></i>
+                    </span>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-lg-4 d-flex align-items-stretch" data-aos="fade-up">
+                        <div class="card">
+                            <img class="card-img-top" src="assets/img/Picture2.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                    card's content.</p>
+                                <a href="posting.php" class="btn btn-primary">Lihat Postingan Lainnya</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+                        <div class="card">
+                            <img class="card-img-top" src="assets/img/Picture4.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                    card's content.</p>
+                                <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 d-flex align-items-stretch" data-aos="fade-up">
+                        <div class="card">
+                            <img class="card-img-top" src="assets/img/Picture3.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                    card's content.</p>
+                                <a href="posting.php" class="btn btn-primary">Lihat Postingan Lainnya</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong><span>LPPM UPR</span></strong>
+            </div>
+            <div class="credits">
+                Designed by <a href="http://lppm.upr.ac.id/" target="_blank">Tim IT LPPM UPR</a>
+            </div>
+        </div>
+    </footer>
+    <!-- End  Footer -->
+
+    <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+    <script src="assets/vendor/counterup/counterup.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/venobox/venobox.min.js"></script>
+    <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="assets/vendor/typed.js/typed.min.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+    <script>
+        var toggler = document.getElementsByClassName("caret");
+        var i;
+
+        for (i = 0; i < toggler.length; i++) {
+            toggler[i].addEventListener("click", function() {
+                this.parentElement.querySelector(".nested").classList.toggle("active");
+                this.classList.toggle("caret-down");
+            });
+        }
+    </script>
+
+</body>
+
+</html>
